5a0decbc300bd63651d572be6dca8c8d6f0ef76c
Shannon Merrell <shannonmerrell@Shannons-MacBook-Pro.local>
2020-04-13 00:51:31 -0400

    pages

--- final_project.py
+++ final_project.py
@@ -1,29 +1,42 @@
 import requests
 import json
 import sqlite3
+import math
 
 conn = sqlite3.connect('movies.db')
 c = conn.cursor()
 
 api_key = "eb392c3f"
 url = 'http://www.omdbapi.com/'
+s = 'marvel'
 
-title = input("Pick a movie:")
-
-url = url + '?T=' + title + '&apikey=' + api_key
-
-print(url)
-
-movie_info = requests.get(url)
+params = {'s':s, 'apikey':api_key}
+movie_info = requests.get(url, params=params)
 # print(f"Status  code: {movie_info.status_code}")
 
 movie_dictionary = movie_info.json()
+page_length = len(movie_dictionary['Search'])
 
-qmarks = ', '.join('?' * len(movie_dictionary))
-qry = "Insert Into Movies (%s) Values (%s)" % (qmarks, qmarks)
-c.execute(qry, movie_dictionary.keys() + movie_dictionary.values())
+total_results = int(movie_dictionary['totalResults'])
 
-print(movie_dictionary.keys())
+pages = math.ceil(total_results/page_length)
+print(pages)
+
+movies_list_combined = []
+
+for page in range(pages):
+    params['page']=page + 1
+    movie_page = requests.get(url, params=params).json()
+    movies_list_combined += movie_page["Search"]
+
+print(len(movies_list_combined))
+
+
+# qmarks = ', '.join('?' * len(movie_dictionary))
+# qry = "Insert Into Movies (%s) Values (%s)" % (qmarks, qmarks)
+# c.execute(qry, movie_dictionary.keys() + movie_dictionary.values())
+
+
 
 
 # google_results = requests.get('http://google.com/search?q=something')
